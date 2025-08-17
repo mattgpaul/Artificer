5e757244cab05ff9107272de1ae535ced5802fbf
Matthew Paul <mattgpaul@gmail.com>
2025-08-17 08:57:57 -0600

    start over

--- services/telemetry-collector/src/main.cpp
+++
@@ -1,8 +0,0 @@
-// main.cpp - Entry point for telemetry collector
-#include "models/telemetry_message.hpp"
-#include "models/cpu/cpu_metrics.hpp"
-#include <iostream>
-
-int main() {
-    return 0;
-}
