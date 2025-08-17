1fdd6d840d03dc10cfdb0ffd763d615db83d6c84
Matthew <34750724+mattgpaul@users.noreply.github.com>
2025-08-16 21:23:27 -0600

    Feat/cpp migration (#3)
    * reset build to use cpp
    
    * setup cpu model

---
+++ services/telemetry-collector/src/main.cpp
@@ -0,0 +1,8 @@
+// main.cpp - Entry point for telemetry collector
+#include "models/telemetry_message.hpp"
+#include "models/cpu/cpu_metrics.hpp"
+#include <iostream>
+
+int main() {
+    return 0;
+}
