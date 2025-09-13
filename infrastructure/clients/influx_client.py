b60d16652ef410a60a729336888e70fe00bac080
Matthew Paul <mattgpaul@gmail.com>
2025-09-13 10:30:44 -0600

    feat: setup influx client

---
+++ infrastructure/clients/influx_client.py
@@ -0,0 +1,62 @@
+import os
+import dotenv
+from datetime import datetime, timezone
+
+from influxdb_client_3 import InfluxDBClient3, Point
+
+from infrastructure.client import Client
+from infrastructure.logging.logger import get_logger
+
+class BaseInfluxDBClient(Client):
+    def __init__(self, database: str):
+        self.logger = get_logger(self.__class__.__name__)
+        dotenv.load_dotenv(dotenv.find_dotenv("artificer.env"))
+        self.token = os.getenv("INFLUXDB_TOKEN")
+        self.header = os.getenv("INFLUXDB_API_HEADER")
+
+        self.host = "localhost"
+        self.port = 8181
+        self.url = f"http://{self.host}:{self.port}"
+
+        self.database = database
+
+    def _create_client(self):
+        self.logger.info("Setting up influx client")
+        self.client = InfluxDBClient3(
+            token=self.token,
+            host=self.url,
+            database=self.database,
+        )
+
+    def write_point(self) -> bool:
+        pass
+
+    def write_points(self) -> bool:
+        pass
+
+    def query_data(self):
+        pass
+
+    def delete_data(self) -> bool:
+        pass
+
+    def ping(self) -> bool:
+        pass
+
+    def health_check(self) -> bool:
+        pass
+
+    def close(self):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+if __name__ == "__main__":
+    influx = BaseInfluxDBClient()
+    influx._create_client()
+    print(f"created client: {influx.client}")
+    
