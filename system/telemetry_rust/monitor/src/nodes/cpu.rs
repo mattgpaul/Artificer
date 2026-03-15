61f16d0b7fd4f4459657a83b11d611c671d57323
Matthew Paul <mattgpaul@gmail.com>
2026-03-14 21:30:05 -0600

    working module calls

--- system/telemetry_rust/monitor/src/nodes/cpu.rs
+++ system/telemetry_rust/monitor/src/nodes/cpu.rs
@@ -1,35 +1,37 @@
-
+enum Manufacturer {
+    Amd,
+    Intel,
+    Apple,
+}
 struct CpuCoreTelemetry {
-    core_num: u8,
-    temp: f64,
+   core_num: usize,
+   temp: f64,
 }
 
-impl CpuCoreTelemetry {
-    // Constructor to set initial values
-    fn new(core_num: u8, temp: f64) -> Self {
-        CpuCoreTelemetry { core_num, temp }
-    }
-
-    // Core getter
-    fn get_core_num(&self) -> u8 {
-        self.core_num
-    }
-
-    // Temp setter
-    fn set_core_temp(&mut self, temp: f64) {
-        self.temp = temp;
-    }
-
-    // Temp getter
-    fn get_core_temp(&self) -> f64 {
-        self.temp
-    }
-
-}
 pub struct CpuTelemetry {
-    manufacturer: String,
-    model: String,
-    num_cores: u8,
-    core_telemetry: Vec<CpuCoreTelemetry>,
+    cores: Vec<CpuCoreTelemetry>,
 }
 
+impl CpuTelemetry {
+    pub fn new() -> Self {
+        let core_count = num_cpus::get();
+        let cores = (0..core_count)
+        .map(|i| CpuCoreTelemetry {core_num: i, temp: 0.0})
+        .collect();
+    CpuTelemetry { cores }    
+    }
+}
+
+#[cfg(target_os = "linux")]
+impl CpuTelemetry {
+    // Refresh telemetry
+    pub fn refresh(&mut self) {
+        self.get_temperature();
+    }
+    // Get temperature telemetry
+    fn get_temperature(&mut self) {
+        for core in &mut self.cores {
+            println!("I got the temps dawg")
+        }
+    }
+}
