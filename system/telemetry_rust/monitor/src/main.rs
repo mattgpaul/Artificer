3c693574e819bd301b3d92026b5868c28102f075
Matthew Paul <mattgpaul@gmail.com>
2026-03-21 22:10:15 -0600

    bug: only returning 0 usage

--- system/telemetry_rust/monitor/src/main.rs
+++ system/telemetry_rust/monitor/src/main.rs
@@ -1,13 +1,16 @@
 mod models;
 mod traits;
-use models::cpu::CpuMonitor;
+use models::cpu::Cpu;
 use crate::traits::telemetry::Telemetry;
 
 fn main() {
-    let mut cpu = CpuMonitor::new();
+    let mut cpu = Cpu::new();
 
     // Main loop
     cpu.refresh();
-    println!("{:?}", cpu)
+    let foo = cpu.get_core_usage();
+    for i in (1..4) {
+        println!("{}",foo[1])
+    }
 }
 
