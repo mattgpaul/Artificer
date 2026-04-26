2b8b882b420c1c9b6eb0a73f788c507a7fd9c0b5
Matthew Paul <mattgpaul@gmail.com>
2026-04-26 13:42:27 -0600

    feat: ram tracking ready

--- system/telemetry_rust/monitor/src/traits/utils.rs
+++ system/telemetry_rust/monitor/src/traits/utils.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::path::PathBuf;
 
+// only works if there is only 1 value to read
 pub fn read_value_from_file(path: &PathBuf) -> Option<u64> {
     if let Ok(contents) = fs::read_to_string(path) {
         if let Ok(value) = contents.trim().parse::<u64>() {
