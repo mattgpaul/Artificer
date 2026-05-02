82d1494d22c4ac4f10569828175e8d2bf115a173
Matthew Paul <mattgpaul@gmail.com>
2026-05-01 20:50:10 -0600

    feat: implement storage refresh

--- system/telemetry_rust/monitor/src/main.rs
+++ system/telemetry_rust/monitor/src/main.rs
@@ -7,6 +7,7 @@ use models::cpu::Cpu;
 use models::gpu::Gpu;
 use models::memory::Memory;
 use models::network::Network;
+use models::storage::Storage;
 use crate::traits::telemetry::Telemetry;
 // ticks in ms
 const TICK: u64 = 1000;
@@ -15,13 +16,15 @@ fn main() {
     let mut gpu = Gpu::new();
     let mut memory = Memory::new();
     let mut network = Network::new();
+    let mut storage = Storage::new();
     // Main loop
     loop {
         cpu.refresh();
         gpu.refresh();
         memory.refresh();
         network.refresh();
-        println!("{:?}", network);
+        storage.refresh();
+        println!("{:?}", storage);
         //sleep
         thread::sleep(Duration::from_millis(TICK));
     }
