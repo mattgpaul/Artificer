07b1c313e40f4f51cd34f733576c22f948597d02
Matthew Paul <mattgpaul@gmail.com>
2026-05-02 20:51:52 -0600

    feat: add service and fix bugs

--- system/telemetry_rust/monitor/src/main.rs
+++ system/telemetry_rust/monitor/src/main.rs
@@ -2,29 +2,17 @@ use std::thread;
 use std::time::Duration;
 
 mod models;
+mod service;
 mod traits;
-use models::cpu::Cpu;
-use models::gpu::Gpu;
-use models::memory::Memory;
-use models::network::Network;
-use models::storage::Storage;
-use crate::traits::telemetry::Telemetry;
+
+use service::Service;
 // ticks in ms
 const TICK: u64 = 1000;
 fn main() {
-    let mut cpu = Cpu::new().expect("Failed to initialize the CPU");
-    let mut gpu = Gpu::new();
-    let mut memory = Memory::new();
-    let mut network = Network::new();
-    let mut storage = Storage::new();
+    let mut monitor = Service::new();
     // Main loop
     loop {
-        cpu.refresh();
-        gpu.refresh();
-        memory.refresh();
-        network.refresh();
-        storage.refresh();
-        println!("{:?}", storage);
+        monitor.tick();
         //sleep
         thread::sleep(Duration::from_millis(TICK));
     }
