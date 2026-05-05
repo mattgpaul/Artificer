6dbb7ce34bcedd989480e97fd43f68d67b2572fc
Matthew Paul <mattgpaul@gmail.com>
2026-05-04 20:28:30 -0600

    feat: port vscode theme to zed

--- system/telemetry_rust/monitor/src/traits/utils.rs
+++ system/telemetry_rust/monitor/src/traits/utils.rs
@@ -1,8 +1,8 @@
 use std::fs;
-use std::path::PathBuf;
+use std::path::Path;
 
 // only works if there is only 1 value to read
-pub fn read_value_from_file(path: &PathBuf) -> Option<u64> {
+pub fn read_value_from_file(path: &Path) -> Option<u64> {
     if let Ok(contents) = fs::read_to_string(path) {
         if let Ok(value) = contents.trim().parse::<u64>() {
             return Some(value);
