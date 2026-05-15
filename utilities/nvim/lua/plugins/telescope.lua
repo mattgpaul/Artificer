70260a59b42b0501e496668e62f8c76cf1c6d588
Matthew Paul <mattgpaul@gmail.com>
2026-05-14 19:39:00 -0600

    feat: track configuration changes to my workflow

--- utilities/nvim/lua/plugins/telescope.lua
+++ utilities/nvim/lua/plugins/telescope.lua
@@ -2,11 +2,19 @@ return {
     'nvim-telescope/telescope.nvim', version = '*',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
+    opts = {
+        defaults = {
+            file_ignore_patterns = {
+                'node_modules/', '%.git/', 'dist/', 'build/', 'target/',
+                '%.next/', '__pycache__/', '%.venv/', 'venv/',
+                '%.lock$', '%.log$',
+            },
+        },
+    },
     keys = {
-        { '<D-p>', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
-        { '<D-h>', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
+        { '<C-p>', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
+        { '<C-S-p>', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
     },
 }
