7a0147628b22f35079d68d89713dca1f17bc8941
Matthew Paul <mattgpaul@gmail.com>
2026-05-24 06:21:13 -0600

    algo trader stack allocation

--- utils/nvim/lua/plugins/autopairs.lua
+++ utils/nvim/lua/plugins/autopairs.lua
@@ -2,8 +2,14 @@ return {
     {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+    config = function()
+        local npairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
+        npairs.setup()
+        npairs.remove_rule("'")
+        npairs.add_rule(Rule("'", "'"):with_pair(function()
+            return vim.bo.filetype ~= "rust"
+        end))
+    end,
     }
 }
