21cdde2fcea5939ca5f5541d1dcd7f5fb7285134
Matthew Paul <mattgpaul@gmail.com>
2026-07-04 02:51:15 +0000

    more home manager setups

--- utils/nixos/users/matthew/home.nix
+++ utils/nixos/users/matthew/home.nix
@@ -1,11 +1,12 @@
-{inputs, pkgs, ... }:
+{ config, inputs, pkgs, ... }:
 {
     imports = [ inputs.home-manager.nixosModules.default ];
 
     home-manager.useGlobalPkgs = true;
     home-manager.useUserPackages = true;
+    home-manager.backupFileExtension = "backup";
 
-    home-manager.users.matthew = {
+    home-manager.users.matthew = { config, pkgs, ... }: {
         home.username = "matthew";
         home.homeDirectory = "/home/matthew";
         home.stateVersion = "26.05";
@@ -16,5 +17,34 @@
         home.packages = with pkgs; [
 # packages here
         ];
+        
+        home.file = let
+            repo = "${config.home.homeDirectory}/Artificer/utils";
+            link = config.lib.file.mkOutOfStoreSymlink;
+        in {
+            ".config/nvim".source = link "${repo}/nvim";
+            ".config/rofi".source = link "${repo}/rofi";
+            ".config/alacritty".source = link "${repo}/alacritty";
+            ".config/hypr".source = link "${repo}/hypr";
+            ".config/waybar".source = link "${repo}/waybar";
+        };
+
+        programs.fzf = {
+            enable = true;
+            enableBashIntegration = true;
+        };
+
+        programs.readline = {
+            enable = true;
+            bindings = {
+                "\\e[A" = "history-search-backward";
+                "\\e[B" = "history-search-forward";
+            };
+            extraConfig = ''
+                set colored-stats on
+                set completion-ignore-case on
+                set show-all-if-ambiguous on
+            '';
+        };
     };
 }
--- utils/nixos/users/matthew/system.nix
+++ utils/nixos/users/matthew/system.nix
@@ -4,16 +4,37 @@
 		isNormalUser = true;
 		extraGroups = [ "wheel" "networkmanager" ];
 		openssh.authorizedKeys.keys = [
-		      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKptSbcOhjFj+FWlW5g19AfNdphHkux6m9IYzwoC7I1c"
-		      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOeqL/GalLrnoIrIFPl5GhYJSNgsrFr7h7+3TW5T9o0"
+			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKptSbcOhjFj+FWlW5g19AfNdphHkux6m9IYzwoC7I1c"
+				"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOeqL/GalLrnoIrIFPl5GhYJSNgsrFr7h7+3TW5T9o0"
 		];
 		packages = with pkgs; [
-        claude-code
-        fzf
-        neovim
-        obsidian
-        jujutsu
-        tree 
-        ];
+			claude-code
+				fzf
+				neovim
+				obsidian
+				jujutsu
+				tree 
+		];
 	};
+
+    programs.bash = {
+        enable = true;
+        shellAliases = {
+            gst = "git status";
+            gco = "git checkout";
+            glg = "git log --oneline --graph --decorate";
+        };
+    };
+
+    programs.git = {
+        enable = true;
+        config = {
+            user.name = "Matthew Paul";
+            user.email = "mattgpaul@gmail.com";
+            init.defaultBranch = "main";
+            push.autoSetupRemote = true;
+            pull.rebase = true;
+            core.editor = "nvim";
+        };
+    };
 }
