08a1cacd83b28f4a66c77faf46199ad6fd4c6ce6
Matthew Paul <mattgpaul@gmail.com>
2026-07-10 17:42:34 +0000

    admin: drop matthew's shared SSH keys (break-glass account, distinct creds)

--- utils/nixos/users/admin/system.nix
+++ utils/nixos/users/admin/system.nix
@@ -3,10 +3,6 @@
 	users.users.admin = {
 		isNormalUser = true;
 		extraGroups = [ "wheel" "networkmanager" ];
-		openssh.authorizedKeys.keys = [
-			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKptSbcOhjFj+FWlW5g19AfNdphHkux6m9IYzwoC7I1c"
-				"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOeqL/GalLrnoIrIFPl5GhYJSNgsrFr7h7+3TW5T9o0"
-		];
 		packages = with pkgs; [
             fzf
             tree 
