fbff35d39205b81a5f61f3216c715745aa87f417
Matthew Paul <mattgpaul@gmail.com>
2026-07-12 19:30:52 +0000

    implementing pulse trait

--- libs/microlib/flake.nix
+++ libs/microlib/flake.nix
@@ -18,6 +18,7 @@
                         cargo
                         clippy
                         rustfmt
+                        rust-analyzer
                     ];
                 };
             });
